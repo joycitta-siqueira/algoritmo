54b3e95cc45277f74e113c101804d243ccde66dc
Joyce Siqueira <joycitta@gmail.com>
2021-03-16 11:31:56 -0300

    Inclusão do Ex01

---
+++ Ex01.cpp
@@ -0,0 +1,15 @@
+//O seu primeiro programa em qualquer linguagem 
+//de programação normalmente é o "Hello World!".
+// Neste primeiro problema tudo o que você precisa 
+//fazer é imprimir esta mensagem na tela.
+//case sensitive : diferenciação entre maiusculos e minusculos
+
+#include<stdio.h>//biblioteca para entrada e saída: printf e scanf
+int main(){ //inicio do algoritmo
+	printf("Hello World!\n\nAula 16/03/2021\n"); //imprima na tela -> \n - quebra de linha
+return 0; // retorno da funçao main
+}//final do algoritmo
+
+
+
+
