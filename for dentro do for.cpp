c1c7776106d3896156c19966e9b29f97663751d9
Joyce Siqueira <joycitta@gmail.com>
2021-05-11 11:29:52 -0300

    Add files via upload

---
+++ for dentro do for.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	
+	int i, j, h;
+	
+    for (instituicao=0; instituicao<3; instituicao++){
+    	for (disciplina=0; disciplina<2; disciplina++){
+	    	for(notas=0;notas<10;notas++){
+	    		scanf("%f", &notas[i]);
+			}
+		}
+	}
+	
+	
+}
+
+
+
+
